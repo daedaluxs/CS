6065290b6fe49b2a241ce8a5a17d5a3b20dcdfd9
daedaluxs <ccgemroll172@gmail.com>
2024-04-18 10:31:56 -0500

    mips

--- mips32.asm
+++ mips32.asm
@@ -1,10 +1,12 @@
 .data 
     somestring: .asciiz "fancywordsmith"
-    matrix1: .word 7, 2   
-             .word 3, 5
-    matrix2: .word 1, 3   
-             .word 2, 1
-    result:  .space 16    # Space for the result (2x2 matrix)
+    matrix1: .word 7, 2, 7 
+             .word 3, 5, 2
+             .word 3, 5, 6
+    matrix2: .word 1, 3, 5
+             .word 2, 1, 2
+             .word 3, 5, 4
+    result:  .space 36    # Space for the result (3x3 matrix)
 .text
     .globl main
 
@@ -15,41 +17,95 @@ main:
     # li $a0, 6 #factorial number
     # jal factorial
 
-    # li $a0, 2 #matrix size
-    # la $a1, matrix2
-    # jal print_matrix
-
-    li $a0, 2
+    li $a0, 3
+    la $a1, matrix1
+    la $a2, matrix2
     jal matrix_mult
 
+    li $a0, 3 #matrix size
+    la $a1, result
+    jal print_matrix
     
 j exit
 matrix_mult:
-    la $t0, matrix1
-    la $t1, matrix2
+    #alternative to this is $sp, allocate with addi sp sp -4x, and access with 4x(sp)
     la $t2, result
     move $t3, $a0 #matrix size
     li $t4, 0 #row
     li $t5, 0 #col
+    li $t7, 0 #inner-inner loop counter
+    li $t8, 0 #for universal / all matrixes sizes lookup
+    li $s0, 0 #total accumulation store
+    li $s1, 0 #matrix1 store
+    li $s2, 0 #matrix2 store
+    li $s3, 0 #multiplication store
 
     j matrix_inner_loop
 
     matrix_outer_loop:
         addi $t4, $t4, 1 #source, destination, amount -- increment outer loop
-        beq $t4, $t3, matrix_end #end when outer hits 2
+        beq $t4, $t3, matrix_end #end when outer hits matrix size
         li $t5, 0 #reset inner loop
 
     matrix_inner_loop:
         #Run loop code here
-        
+        li $t7, 0 #inner inner loop's counter
+        li $s0, 0 #reset mult stored value
+        inner_inner_loop:
+            #MATRIX2 - use col, incrmeent row
+            move $t0, $t7
+            move $t1, $t5 
+            mul $t8, $t3, 4 #is the multiplier for the row
+            mul $t0, $t0, $t8 #mult8 by 8 for 2x2, 12 for 3x3
+            sll $t1, $t1, 2 #shift left by 2 bits, aka mult by 4
+
+            add $t0, $t0, $t1 #combine row.col values, stored in t0
+            add $t0, $t0, $a2 #add offset to the matrix2 mem addr
+
+            lw $s2, ($t0) #getting the value at the position of the matrix 
+
+            #MATRIX1 - use row, increment col
+            move $t0, $t4
+            move $t1, $t7 
+            mul $t8, $t3, 4 #is the multiplier for the row
+            mul $t0, $t0, $t8 #mult8 by 8 for 2x2, 12 for 3x3
+            sll $t1, $t1, 2 #shift left by 2 bits, aka mult by 4
+
+            add $t0, $t0, $t1 #combine row.col values, stored in t0
+            add $t0, $t0, $a1 #add offset to the matrix1 mem addr
+
+            lw $s1, ($t0) #getting the value at the position of the matrix 
+
+            #-=- -=-
+
+            mul $s3, $s1, $s2 #multiply the 2 values from matrix1 and 2 together
+
+            add $s0, $s0, $s3 #combining values from both matrices
+
+            addi $t7, $t7, 1
+            beq $t7, $t3, matrix_inner_jump #we dont want to just.. restart the loop when we're done
+
+            j inner_inner_loop
+
+        matrix_inner_jump:
+        move $t0, $t4
+        move $t1, $t5
+        mul $t8, $t3, 4 #is the multiplier for the row
+        mul $t0, $t0, $t8 #mult8 by 8 for 2x2, 12 for 3x3
+        sll $t1, $t1, 2 #shift left by 2 bits, aka mult by 4
+
+        add $t0, $t0, $t1 #combine row.col values, stored in t0
+        add $t0, $t0, $t2 #add offset to the matrix mem addr
+
+        sw $s0, ($t0)
         #-=-=-
         addi $t5, $t5, 1
-        beq $t5, $t3, matrix_outer_loop #inner hits 2, increment outer
+        beq $t5, $t3, matrix_outer_loop #inner (col) hits size(2,3,etc), increment outer (row)
         
         j matrix_inner_loop
 
     matrix_end:
-        jr $ra
+    jr $ra
 
 print_matrix:
     move $t8, $ra #;p
@@ -57,6 +113,7 @@ print_matrix:
     li $t2, 0 #row
     li $t3, 0 #col
     move $t0, $a1 #address of matrix to t0
+    li $t6, 0
 
     j pm_inner_loop
 
@@ -66,9 +123,7 @@ print_matrix:
         li $t3, 0 #reset inner loop
 
     pm_inner_loop:
-        #Run loop code here
-
-        jal pw
+        jal pw #looping the print functionality on every index
         
         #-=-=-
         addi $t3, $t3, 1
@@ -81,14 +136,17 @@ print_matrix:
         #making copies
         move $t4, $t2
         move $t5, $t3
-        sll $t4, $t4, 3 #shift left by 3 bits, aka mult by 8
+
+        mul $t9, $t1, 4 #is the multiplier for the row
+        mul $t4, $t4, $t9 #mult8 by 8 for 2x2, 12 for 3x3
+
         sll $t5, $t5, 2 #shift left by 2 bits, aka mult by 4
 
-        add $t4, $t4, $t5 #combine row.col values, stored in t4
+        add $t4, $t4, $t5 #combine row+col values, stored in t4
         add $t4, $t4, $t0 #add offset to the matrix mem addr
-        li $v0, 1
 
-        lw $a0, ($t4)
+        li $v0, 1
+        lw $a0, ($t4) #a0 reg to print, loaded from matrix
         syscall
 
         li $v0, 11 #print character
@@ -96,7 +154,7 @@ print_matrix:
         syscall
         
         subi $t6, $t1, 1
-        beq $t3, $t6, newline
+        beq $t3, $t6, newline #if our current col counter is printing the last row(col==size), print newline
 
         jr $t7
 
